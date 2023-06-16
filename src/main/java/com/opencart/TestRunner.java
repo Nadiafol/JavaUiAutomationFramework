3aaa7baeec7bec2f100ac1aea174d185dec45a0b
NadiaFol <foltea.nadejda@hotmail.com>
2023-06-16 20:42:24 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,27 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        // one way to define a driver manager
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java/4.9.1");
+        Thread.sleep(2000);
+        String cardCurrentWindowName = driver.getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F%3Ftrk%3Dguest_homepage-basic_sign-in-submit&trk=login_reg_redirect");
+        Thread.sleep(2000);
+        driver.close();
+        driver.switchTo().window(cardCurrentWindowName);
+        driver.get("https://translate.google.com/?sl=auto&tl=ro&text=spare&op=translate&hl=ro");
+        Thread.sleep(2000);
+        driver.quit();
+
+        System.out.println("The execution was finished.");
     }
 }
