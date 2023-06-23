fb99b04f33b42dc048be9e82fdb6e0e9b28572e2
NadiaFol <foltea.nadejda@hotmail.com>
2023-06-23 18:02:46 +0300

    Implemented the Page Factory template, login and logout test scenario.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -3,60 +3,42 @@ package com.opencart;
 import com.opencart.managers.DataFakeManager;
 import com.opencart.managers.DriverManager;
 
-import com.opencart.managers.ScrollManager;
+import com.opencart.pageobjects.AccountCreatedPage;
+import com.opencart.pageobjects.HomePage;
+import com.opencart.pageobjects.RegisterPage;
 import org.openqa.selenium.*;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
         // one way to define a driver manager
         WebDriver driver = DriverManager.getInstance().getDriver();
-        driver.get("https://www.google.com/");
-
-        String currentWindowName = driver.getWindowHandle();
-        driver.switchTo().newWindow(WindowType.TAB);
+        
         driver.get("https://andreisecuqa.host/");
         Thread.sleep(500);
 
-        WebElement accountIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
-        accountIcon.click();
-        Thread.sleep(500);
-        WebElement registerButton = driver.findElement(By.xpath("//a[normalize-space()='Register']"));
-        registerButton.click();
-        Thread.sleep(500);
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegisterPageFromHeaderMenu();
+
 
         String firstName = DataFakeManager.getRandomName();
-        System.out.println("The generated first name is: " + firstName);
         String lastName = DataFakeManager.getRandomName();
-        System.out.println("The generated last name is: " + lastName);
         String email = DataFakeManager.getRandomEmail();
-        System.out.println("The generated email is: " + email);
         String password = DataFakeManager.getPassword(5, 24);
-        System.out.println("The generated password is: " + password);
 
-        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
-        firstNameInput.sendKeys(firstName);
-        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
-        lastNameInput.sendKeys(lastName);
-        WebElement emailInput = driver.findElement(By.id("input-email"));
-        emailInput.sendKeys(email);
-        WebElement passwordInput = driver.findElement(By.id("input-password"));
-        passwordInput.sendKeys(password);
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.fillInTheRegisterForm(firstName, lastName, email, password);
+        registerPage.switchOnThePrivacyToggle(driver);
+        registerPage.clickOnTheContinueButton();
 
-        WebElement privacyToggle = driver.findElement(By.cssSelector("input[value='1'][name='agree']"));
-        ScrollManager.scrollToElement(driver, privacyToggle);
-        privacyToggle.click();
-        Thread.sleep(1000);
-
-        WebElement continueButton = driver.findElement(By.xpath("//button[normalize-space()='Continue']"));
-        continueButton.click();
-        Thread.sleep(1000);
-
+        Thread.sleep(2000);
         System.out.println(driver.getCurrentUrl());
-        driver.close();
-        Thread.sleep(1000);
 
-        driver.switchTo().window(currentWindowName);
-        driver.get("https://andreisecuqa.host/");
+        AccountCreatedPage accountCreatedPage = new AccountCreatedPage(driver);
+        accountCreatedPage.logoutFromTheAccount();
+
+        Thread.sleep(2000);
+        System.out.println(driver.getCurrentUrl());
+
         driver.quit();
 
         System.out.println("The execution was finished.");
